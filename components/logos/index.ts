df01513e71f9aa3e14e64a3e2e9bc90859db2faa
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-03-04 22:36:21 +0700

    Initial Landing Page Coregy

---
+++ components/logos/index.ts
@@ -0,0 +1,3 @@
+export * from './github'
+export * from './openai'
+export * from './react'
---
+++ theme/index.ts
@@ -0,0 +1,49 @@
+import { extendTheme } from '@chakra-ui/react'
+import '@fontsource-variable/inter'
+import { theme as baseTheme } from '@saas-ui/react'
+
+import components from './components'
+import { fontSizes } from './foundations/typography'
+
+export const theme = extendTheme(
+  {
+    config: {
+      initialColorMode: 'dark',
+      useSystemColorMode: false,
+    },
+    styles: {
+      global: (props: any) => ({
+        body: {
+          color: 'gray.900',
+          bg: 'white',
+          fontSize: 'lg',
+          _dark: {
+            color: 'white',
+            bg: 'gray.900',
+          },
+        },
+      }),
+    },
+    fonts: {
+      heading: 'Inter Variable, Inter, sans-serif',
+      body: 'Inter Variable, Inter, sans-serif',
+    },
+    fontSizes,
+    colors: {
+      primary: {
+        50: '#E3F2FD',
+        100: '#BBDEFB',
+        200: '#90CAF9',
+        300: '#64B5F6',
+        400: '#42A5F5',
+        500: '#0D47A1', // Màu chính
+        600: '#1565C0', // Xanh đậm hơn
+        700: '#0D47A1', // Xanh navy
+        800: '#0B3D91',
+        900: '#072D74',
+      },
+    },
+    components,
+  },
+  baseTheme,
+)
